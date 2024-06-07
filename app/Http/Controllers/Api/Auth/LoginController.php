d9105e7f7b85633b3086b9a78bd3e7b4ece04b7a
ReemAhmad2 <reemahmadn22@gmail.com>
2024-06-07 11:02:53 +0300

    login

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Traits\GeneralTrait;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    use GeneralTrait;
+
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => ['required', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:8'],
+        ]);
+
+        if ($validator->fails()) {
+            return $this->apiResponse(null, false, $validator->errors(), 422);
+        }
+
+        try {
+            $user = User::where('email', $request->email)->first();
+
+            if ($user && Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('web')->plainTextToken;
+
+                return $this->apiResponse([
+                    'access_token' => $token,
+                    'token_type' => 'Bearer',
+                    'type' => $user->type
+                ]);
+            } else {
+                return $this->apiResponse(null, false, 'Invalid email or password', 401);
+            }
+        } catch (\Exception $e) {
+            return $this->apiResponse(null, false, $e->getMessage(), 500);
+        }
+    }
+}
